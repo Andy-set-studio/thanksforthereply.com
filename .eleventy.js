ceed7bf4aa264eb46c2ba0c113bf10d37f83855d
Andy Bell <me@andy-bell.co.uk>
2020-02-10 20:05:26 +0000

    Add icons etc

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,8 @@ module.exports = config => {
   config.addPassthroughCopy('./src/fonts');
   config.addPassthroughCopy('./src/images');
   config.addPassthroughCopy('./src/js');
+  config.addPassthroughCopy('./src/site.webmanifest');
+  config.addPassthroughCopy('./src/browserconfig.xml');
 
   return {
     dir: {
